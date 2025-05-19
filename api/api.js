af6b257f97a74e94cd11ee20f137e77f817a0542
gustavo peretti <gustavo.peretti2001@gmail.com>
2025-05-19 11:46:03 +0100

    chore: enable CORS so frontend can fetch from API

--- api/api.js
+++ api/api.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const endpoints = require('../endpoints.json');
+const cors = require("cors");
+const express = require("express");
+const endpoints = require("../endpoints.json");
 const {
   getTopics,
   getArticleById,
@@ -8,34 +9,33 @@ const {
   postCommentByArticleId,
   patchArticleById,
   deleteCommentById,
-  getUsers
-} = require('./controller/nc_news.controller');
+  getUsers,
+} = require("./controller/nc_news.controller");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   res.status(200).send({ endpoints });
 });
 
-app.get('/api/topics', getTopics);
-app.get('/api/articles/:article_id', getArticleById);
-app.get('/api/articles', getArticles);
-app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
-app.post('/api/articles/:article_id/comments', postCommentByArticleId);
-app.patch('/api/articles/:article_id', patchArticleById);
-app.delete('/api/comments/:comment_id', deleteCommentById);
-app.get('/api/users', getUsers);
+app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
+app.post("/api/articles/:article_id/comments", postCommentByArticleId);
+app.patch("/api/articles/:article_id", patchArticleById);
+app.delete("/api/comments/:comment_id", deleteCommentById);
+app.get("/api/users", getUsers);
 
-
-app.all('*splat', (req, res, next) => {
-  next({ status: 404, msg: '404: Not Found' });
+app.all("*splat", (req, res, next) => {
+  next({ status: 404, msg: "404: Not Found" });
 });
 
 app.use((err, req, res, next) => {
-  if (err.code === '22P02') {
-    return res.status(400).send({ msg: 'bad request' });
+  if (err.code === "22P02") {
+    return res.status(400).send({ msg: "bad request" });
   } else if (err.status && err.msg) {
     return res.status(err.status).send({ msg: err.msg });
   }
@@ -44,7 +44,7 @@ app.use((err, req, res, next) => {
 
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).send({ msg: 'Internal server error' });
+  res.status(500).send({ msg: "Internal server error" });
 });
 
 module.exports = app;
